46f6af8031edfdd001b786f140456a065daa78c4
Kieran Simpson <kieran@redcrew.com.au>
2021-10-21 16:15:24 +1100

    Add pretend toolbar.

--- app/src/main/java/au/com/wpay/sdk/paymentsimulator/MainActivity.kt
+++ app/src/main/java/au/com/wpay/sdk/paymentsimulator/MainActivity.kt
@@ -1,13 +1,24 @@
 package au.com.wpay.sdk.paymentsimulator
 
+import android.content.Context
 import android.os.Bundle
+import android.util.TypedValue
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import au.com.wpay.sdk.paymentsimulator.ui.theme.WPayPaymentSimulatorAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,22 +28,54 @@ class MainActivity : ComponentActivity() {
             WPayPaymentSimulatorAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Column(
+                        modifier = Modifier.fillMaxHeight()
+                    ) {
+                        Toolbar(dimensionResource(id = getActionBarSize()))
+                    }
                 }
             }
         }
     }
+
+    // TODO: Figure out better way to get attr value in Compose
+    private fun getActionBarSize(): Int =
+        getResIdFromAttribute(this, android.R.attr.actionBarSize)
+}
+
+fun getResIdFromAttribute(context: Context, attr: Int): Int {
+    if (attr == 0) {
+        return 0
+    }
+
+    val typedValue = TypedValue()
+    context.theme.resolveAttribute(attr, typedValue, true)
+
+    return typedValue.resourceId
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Toolbar(height: Dp) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(height)
+            .background(Color.White),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.wpay_logo),
+            contentDescription = "",
+            modifier = Modifier.scale(0.7f)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     WPayPaymentSimulatorAppTheme {
-        Greeting("Android")
+        Toolbar(Dp(56.0f))
     }
 }
