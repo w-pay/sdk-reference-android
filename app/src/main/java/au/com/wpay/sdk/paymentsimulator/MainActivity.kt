e779dea4040e10f58678ea0c6e5696dffdbc764d
Kieran Simpson <kieran@redcrew.com.au>
2021-10-26 09:24:28 +1100

    Adjust layout of activity

--- app/src/main/java/au/com/wpay/sdk/paymentsimulator/MainActivity.kt
+++ app/src/main/java/au/com/wpay/sdk/paymentsimulator/MainActivity.kt
@@ -4,25 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import au.com.wpay.sdk.paymentsimulator.ui.theme.WPayPaymentSimulatorAppTheme
 
+@ExperimentalMaterialApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,12 +30,14 @@ class MainActivity : ComponentActivity() {
             WPayPaymentSimulatorAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Column(
+                    Scaffold(
                         modifier = Modifier.fillMaxHeight()
                     ) {
-                        Toolbar()
-                        NavHost(navController = navController, startDestination = "settings") {
-                            composable("settings") { WPaySettings() }
+                        Column {
+                            Toolbar()
+                            NavHost(navController = navController, startDestination = "settings") {
+                                composable("settings") { WPaySettings() }
+                            }
                         }
                     }
                 }
@@ -54,8 +53,7 @@ fun Toolbar() {
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .fillMaxWidth()
-            .height(Dp(70f))
-            .background(Color.White)
+            .height(100.dp)
     ) {
         Column {
             Image(
@@ -64,6 +62,7 @@ fun Toolbar() {
                 modifier = Modifier
                     .fillMaxWidth()
                     .scale(0.7f)
+                    .padding(0.dp, 20.dp, 0.dp, 0.dp)
             )
 
             Text(
