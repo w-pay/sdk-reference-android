66b2283fd3479449fb54b4bded8b59ca997e5ccd
Kieran Simpson <kieran@redcrew.com.au>
2021-10-26 09:22:56 +1100

    Adjust theme for light and dark mode.

--- app/src/main/java/au/com/wpay/sdk/paymentsimulator/ui/theme/Theme.kt
+++ app/src/main/java/au/com/wpay/sdk/paymentsimulator/ui/theme/Theme.kt
@@ -5,26 +5,24 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
     background = Color.White,
     surface = Color.White,
-    onPrimary = Color.White,
+    onPrimary = Color.Black,
     onSecondary = Color.Black,
     onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    onSurface = Color.Black
+)
+
+private val ColorPalette = lightColors(
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.Black,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black
 )
 
 @Composable
@@ -32,11 +30,13 @@ fun WPayPaymentSimulatorAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    }
-    else {
-        LightColorPalette
+    val colors = when {
+        darkTheme -> {
+            DarkColorPalette
+        }
+        else -> {
+            ColorPalette
+        }
     }
 
     MaterialTheme(
